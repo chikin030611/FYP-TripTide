4fecc4760df78196572c4942fa9dac54a97f8f98
Chi Kin Tang <chikin030611@gmail.com>
2024-09-26 23:07:37 +0800

    Initial Commit

---
+++ FYP-TripTide/FYP_TripTideApp.swift
@@ -0,0 +1,17 @@
+//
+//  FYP_TripTideApp.swift
+//  FYP-TripTide
+//
+//  Created by Chi Kin Tang on 26/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct FYP_TripTideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FYP-TripTideTests/FYP_TripTideTests.swift
@@ -0,0 +1,17 @@
+//
+//  FYP_TripTideTests.swift
+//  FYP-TripTideTests
+//
+//  Created by Chi Kin Tang on 26/9/2024.
+//
+
+import Testing
+@testable import FYP_TripTide
+
+struct FYP_TripTideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
