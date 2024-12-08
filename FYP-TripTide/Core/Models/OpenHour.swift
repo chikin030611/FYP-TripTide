3563bb7828071b69bd348e4b499cc2e1dc13e52a
Chi Kin Tang <chikin030611@gmail.com>
2024-12-08 23:51:45 +0800

    Organize the project

--- FYP-TripTide/Core/Models/OpenHour.swift
+++ FYP-TripTide/Core/Models/OpenHour.swift
@@ -1,53 +1,10 @@
-//
-//  Models.swift
-//  FYP-TripTide
-//
-//  Created by Chi Kin Tang on 16/11/2024.
-//
-
 import Foundation
 
-// MARK: - Attraction Model
-struct Attraction: Identifiable {
-    var id: String
-    var images: [String]
-    var name: String
-    var rating: Int
-    var price: String
-    var tags: [Tag]
-    var openHours: [OpenHour]
-    var stayingTime: String
-    var description: String
-    var latitude: Double
-    var longitude: Double
-}
-
-extension Attraction {
-    static let empty = Attraction(
-        id: "",
-        images: [],
-        name: "",
-        rating: 0,
-        price: "",
-        tags: [],
-        openHours: [],
-        stayingTime: "",
-        description: "",
-        latitude: 0,
-        longitude: 0
-    )
-}
-
-// MARK: - Tag Model
-struct Tag: Equatable {
-    var name: String
-}
-
-// MARK: - OpenHour Model
 struct OpenHour {
     var weekdayIndex: Int
     var openTime: String?
     var closeTime: String?
+    
     var isRestDay: Bool {
         return openTime == nil || closeTime == nil
     }
@@ -55,7 +12,7 @@ struct OpenHour {
     func dayName() -> String {
         let formatter = DateFormatter()
         formatter.locale = Locale.current
-        return formatter.weekdaySymbols[weekdayIndex - 1] // Array is 0-based, weekdayIndex is 1-based
+        return formatter.weekdaySymbols[weekdayIndex - 1]
     }
     
     func getOpenTimeHour() -> Int? {
@@ -77,13 +34,16 @@ struct OpenHour {
         guard let closeTime = closeTime else { return nil }
         return OpenHour.convertTimeToMinute(time: closeTime)
     }
-    
+}
+
+// MARK: - Time Conversion Helpers
+extension OpenHour {
     static func convertTimeToHour(time: String) -> Int? {
         let formatter = DateFormatter()
-        formatter.dateFormat = "HH:mm" // 24-hour time format
+        formatter.dateFormat = "HH:mm"
         
         guard let date = formatter.date(from: time) else {
-            return nil // Return nil if the date conversion fails
+            return nil
         }
         
         let calendar = Calendar.current
@@ -92,15 +52,13 @@ struct OpenHour {
 
     static func convertTimeToMinute(time: String) -> Int? {
         let formatter = DateFormatter()
-        formatter.dateFormat = "HH:mm" // 24-hour time format
+        formatter.dateFormat = "HH:mm"
         
         guard let date = formatter.date(from: time) else {
-            return nil // Return nil if the date conversion fails
+            return nil
         }
         
         let calendar = Calendar.current
         return calendar.component(.minute, from: date)
     }
-}
-
-
+} 
---
+++ FYP-TripTide/Core/Models/Tag.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+struct Tag: Equatable {
+    var name: String
+} 
