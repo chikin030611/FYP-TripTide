b99d6f57a919e55012e4d0a2682e2fc1dbac9ef0
Chi Kin Tang <chikin030611@gmail.com>
2024-12-15 22:56:53 +0800

    Create Components folder for Tip

---
+++ FYP-TripTide/Features/Tips/Components/TipCardView.swift
@@ -0,0 +1,42 @@
+import SwiftUI
+
+
+struct TipCardView: View {
+    let tip: Tip
+    @StateObject private var themeManager = ThemeManager()
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            AsyncImage(url: URL(string: tip.coverImage)) { image in
+                image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 150)
+                    .clipped()
+            } placeholder: {
+                ProgressView()
+            }
+            .cornerRadius(10)
+            
+            Text(tip.title)
+                .font(themeManager.selectedTheme.titleFont)
+                .foregroundStyle(themeManager.selectedTheme.primaryColor)
+            
+            HStack {
+
+                ForEach(tip.author.split(separator: ","), id: \.self) { author in
+                    HStack(spacing: 4) {
+                        Text("By")
+                            .font(themeManager.selectedTheme.captionTextFont)
+                        Text(author)
+                            .foregroundStyle(author == "TripTide" ? themeManager.selectedTheme.accentColor : .primary)
+                    }
+                }
+                Spacer()
+                Text(tip.publishDate.formatted(date: .abbreviated, time: .omitted))
+            }
+            .font(themeManager.selectedTheme.captionTextFont)
+            .foregroundStyle(themeManager.selectedTheme.secondaryColor)
+        }
+    }
+}
