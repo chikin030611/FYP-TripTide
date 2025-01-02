9bf50b921caaf5a66ba9864a0af00b5e01717bd8
Chi Kin Tang <chikin030611@gmail.com>
2025-01-02 21:42:24 +0800

    Update UserTabView.swift

--- FYP-TripTide/Features/User/Views/UserTabView.swift
+++ FYP-TripTide/Features/User/Views/UserTabView.swift
@@ -3,14 +3,24 @@ import SwiftUI
 struct UserTabView: View {
     @State private var showLoginSheet = false
     @StateObject private var authManager = AuthManager.shared
+    @State private var isCheckingAuth = true
     
     var body: some View {
         NavigationView {
-            if authManager.isAuthenticated {
-                UserProfileView()
-            } else {
-                LoginView()
+            Group {
+                if isCheckingAuth {
+                    ProgressView() // Show loading while checking auth
+                } else if authManager.isAuthenticated {
+                    UserProfileView()
+                } else {
+                    LoginView()
+                }
             }
+        }
+        .task {
+            // Validate token when view appears
+            await authManager.validateToken()
+            isCheckingAuth = false
         }
     }
 }
